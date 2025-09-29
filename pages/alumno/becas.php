6a3ce96cfedae5e983bdd113fc06d9ecbe5cd5ab
jesushernandez5656 <jesushernandezhernandez0925@gmail.com>
2025-09-29 10:50:44 -0600

    css

--- pages/alumno/becas.php
+++ pages/alumno/becas.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/UTPN/assets/css/becas.css">
-    <link rel="stylesheet" href="/UTPN/assets/css/header.css">
-    <link rel="stylesheet" href="/UTPN/assets/css/footer.css">
+    <link rel="stylesheet" href="/integradora-UTPN/assets/css/becas.css">
+    <link rel="stylesheet" href="/integradora-UTPN/assets/css/header.css">
+    <link rel="stylesheet" href="/integradora-UTPN/assets/css/footer.css">
     <title>Document</title>
 </head>
 <body>
