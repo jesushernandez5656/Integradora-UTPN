9cb08e9512b536108b1c6bc8370d994f4ad60e32
omarIRCM <omarx1060@gmail.com>
2025-11-24 08:58:39 -0700

    A ver si lo sube
    A ver si sube este mugrero

--- pages/alumno/ChatBot.php
+++ pages/alumno/ChatBot.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $input = $_POST["message"];
     $config = include "../api/config.php";
     $apiKey = $config["apiKey"];
- 
+  
     $ch = curl_init("https://api.openai.com/v1/chat/completions");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
