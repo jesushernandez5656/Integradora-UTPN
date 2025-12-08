1e0144ad0562048a16d8652f55cefd73c2033c1a
jesushernandez5656 <jesushernandezhernandez0925@gmail.com>
2025-12-08 11:44:15 -0700

    asd

--- pages/alumno/api/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-return [
-    "apiKey" => "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"
-];
