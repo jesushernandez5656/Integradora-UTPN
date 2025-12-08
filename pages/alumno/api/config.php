f5156d67669804a62fcef1f4dac1a05822b7a083
jesushernandez5656 <jesushernandezhernandez0925@gmail.com>
2025-12-08 11:39:01 -0700

    Guardar

---
+++ pages/alumno/api/config.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    "apiKey" => "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"
+];
