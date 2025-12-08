19b8f93428756ccc3940ff80474875814cace3fc
jesushernandez5656 <jesushernandezhernandez0925@gmail.com>
2025-12-08 10:31:37 -0700

    Add files via upload

---
+++ pages/api/config.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    "apiKey" => "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"
+];
