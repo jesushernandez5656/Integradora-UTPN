53dc61b3a88fe4f23112c78064a0fa562ecd18ad
jesushernandez5656 <jesushernandezhernandez0925@gmail.com>
2025-12-08 13:19:23 -0700

    asd

--- pages/admin/home_admin.php
+++ pages/admin/home_admin.php
@@ -128,7 +128,7 @@ body {
         </a>
 
         <!-- 📌 MAPA -->
-        <a href="<?= tienePermiso('mapa') ? '../../pages/alumno/recursos-aprendizaje/index.php' : '#' ?>">
+        <a href="<?= tienePermiso('mapa') ? '../admin/admin_mapa_calendario.php' : '#' ?>">
             <div class="logo <?= !tienePermiso('mapa') ? 'disabled' : '' ?>">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide-map">
                     <path d="M14.106 5.553a2 2 0 0 0 1.788 0l3.659-1.83A1 1 0 0 1 21 4.619v12.764a1 1 0 0 1-.553.894l-4.553 2.277a2 2 0 0 1-1.788 0l-4.212-2.106a2 2 0 0 0-1.788 0l-3.659 1.83A1 1 0 0 1 3 19.381V6.618a1 1 0 0 1 .553-.894l4.553-2.277a2 2 0 0 1 1.788 0z"/>
