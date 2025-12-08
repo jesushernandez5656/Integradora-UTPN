ee70d31d3558574a656c92b433f6e42b835f4cda
jesushernandez5656 <jesushernandezhernandez0925@gmail.com>
2025-12-08 11:04:20 -0700

    hello

--- pages/api/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-return [
-    "apiKey" => "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"
-];
